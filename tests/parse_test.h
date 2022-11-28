5d5dee9ce6c8bc5694e0fb0f2c2844c2d8cb46b8
SiddantY <sky2@illinois.edu>
2022-11-28 04:00:11 +0000

    Removed Useless

--- tests/parse_test.h
+++
@@ -1,5 +0,0 @@
-#include <catch2/catch_test_macros.hpp>
-#include "/workspaces/CS 225/CS225/CS225-FINAL-PROJECT-FA22/src/graph.h"
-#include <map>
-#include <algorithm>
-bool compareMap(std::map<std::string, std::vector<std::string>> m1, std::map<std::string, std::vector<std::string>> m2);
--- tests/test_helper.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <map>
-#include <vector>
-#include <string>
-
-bool compareMap(std::map<std::string, std::vector<std::string>> m1, std::map<std::string, std::vector<std::string>> m2);
